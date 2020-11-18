dd02cd41cf0f4dca9e516f191020839d7a6da693
spajda65 <74651489+spajda65@users.noreply.github.com>
2020-11-18 10:34:40 +0100

    Update Form1.cs

--- jetotak/Form1.cs
+++ jetotak/Form1.cs
@@ -24,6 +24,7 @@ namespace jetotak
 
             label1.Text = (a + b).ToString();
             label2.Text = (a - b).ToString();
+            label3.Text = (a * b).ToString();
 
             if (b != 0) label4.Text = (a / b).ToString();
             else label4.Text = " nelze ";
