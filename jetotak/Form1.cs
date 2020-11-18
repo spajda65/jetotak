404af7ffc74b6f1f4cf733e2177f9c8bf813a33c
Tomáš Špaček <tomasek.spacek@seznam.cz>
2020-11-18 10:27:25 +0100

    +-*

--- jetotak/Form1.cs
+++ jetotak/Form1.cs
@@ -23,7 +23,9 @@ namespace jetotak
             int b = Convert.ToInt32(textBox2);
 
             label1.Text = (a + b).ToString();
-            
+            label2.Text = (a - b).ToString();
+            label3.Text = (a * b).ToString();
+
             if (b != 0) label4.Text = (a / b).ToString();
             else label4.Text = " nelze ";
         }
